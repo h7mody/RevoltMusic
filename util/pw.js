d6b50ea418dda5b497ac5551369129bb7714b4d7
h7mody <122606097+h7mody@users.noreply.github.com>
2024-02-19 00:47:03 +0400

    Update pw.js

--- util/pw.js
+++ util/pw.js
@@ -13,7 +13,7 @@ function printWatermark() {
   console.log(`\x1b[1m\x1b[36m            👑 Authorization : ${status}    `);
   console.log(`\x1b[1m\x1b[36m            💡 Version: ${version}`);
   console.log(`\x1b[1m\x1b[36m            📅 Uptime: ${uptimeInSeconds}s`);
-  console.log(`\x1b[1m\x1b[36m            🚀 Powered by ${2f0.}`);
+  console.log(`\x1b[1m\x1b[36m            🚀 Powered by ${RTX}`);
   console.log('\x1b[1m\x1b[36m║                                            ║');
   console.log('\x1b[1m\x1b[36m╚════════════════════════════════════════════╝\x1b[0m');
 }
